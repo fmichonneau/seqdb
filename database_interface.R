e657559f84a8c0eaf22a835a3a99c595a1a2bcbe
Francois Michonneau <francois.michonneau@gmail.com>
2015-04-07 18:30:59 -0400

    edits after importing second batch of plates

--- database_interface.R
+++ database_interface.R
@@ -1,8 +1,6 @@
 
 library(RSQLite)
 
-
-
 db_structure <- list(
     plate_data =
         "CREATE TABLE `plate_data` (
@@ -27,14 +25,15 @@ db_structure <- list(
         );",
     sequence_info =
         "CREATE TABLE `sequence_info` (
-           `sequence_plate`     text,
-           `sequence_row`	text,
-	   `sequence_column`	text,
-           `sequence_valid`     boolean,
+           `sequence_plate`        text,
+           `sequence_row`	   text,
+	   `sequence_column`	   text,
+           `matrix_plate_barcode`  text,
+           `sequence_successful`   boolean,
            `sequence_contaminated` boolean,
            `sequence_pseudogene`   boolean,
            `sequence_note`         text,
-           FOREIGN KEY (sequence_plate, sequence_row, sequence_column)  REFERENCES plate_data(plate, row, column)
+         FOREIGN KEY (sequence_plate, sequence_row, sequence_column)  REFERENCES plate_data(plate, row, column)
         );",
     dictionary_phyla_id =
         "CREATE TABLE `dictionary_phyla_id` (
@@ -43,11 +42,12 @@ db_structure <- list(
         );",
     plate_tracking =
         "CREATE TABLE `plate_tracking` (
-	`plate`	TEXT,
+	`plate`	        TEXT,
 	`is_full`	TEXT DEFAULT '(null)',
 	`sent_on`	TEXT,
 	`received_on`	TEXT,
-	`extracted_on`	TEXT
+	`extracted_on`	TEXT,
+        `notes`         TEXT
         );")
 
     ## ufdb =
@@ -157,21 +157,39 @@ import_fasta <- function(file, prefix="FMSLI13", db_prefix="FMSL13_plate_",
 }
 
 
+import_annotations <- function(file, db_prefix="FMSL13_", ...) {
+    ann <- read.csv(file=file)
 
-con <- dbConnect(SQLite(), "lines_sequences.sqlite")
+    data.frame(sequence_plate= paste0(db_prefix, gsub("^0{1}", "", res[, 1])),
+               sequence_row=,
+               sequence_column=)
+
+}
+
+
+con <- dbConnect(SQLite(), "20150407.lines_sequences.sqlite")
 
 create_database(con, db_structure)
 
-add_data(con, table="plate_data", file="importedData/plate_data.csv")
-add_data(con, table="plate_tracking", file="importedData/plate_tracking.csv")
+plate_data <- list.files(path="importedData/", pattern="^plate_(.+)csv$",
+                         full.names=TRUE)
+lapply(plate_data, function(x) add_data(con, table="plate_data", file=x))
+add_data(con, table="plate_tracking", file="importedData/tracking_info.csv")
 add_data(con, table="dictionary_phyla_id", file="importedData/dictionary_phyla_id.csv")
-dbWriteTable(conn=con, value=read.csv("importedData/ufdb.csv"), name="ufdb",
+dbWriteTable(conn=con, value=read.csv("20150407-all_uf.csv"), name="ufdb",
              row.names=FALSE, overwrite=TRUE, header=TRUE)
 import_fasta(file="fromMatt/FMSLI13_1-6.fasta", con=con)
 
 dbDisconnect(con)
 
 
+####
+
+## sli_st <- read_excel("~/Documents/2013-10.Lines/2013-10.Lines_intermediateCataloging_phase2.xls")
+## fm13 <- read.csv(file = "importedData/plate_FMSL13_13.csv", stringsAsFactors = FALSE)
+
+## tt <- left_join(fm13, sli_st[, c("Field_#", "UF_ID")], by = c("ufid_unique" =  "Field_#"))
+## write.csv(tt, file = "/tmp/fixUFID.csv")
 
 
 
@@ -179,13 +197,17 @@ dbDisconnect(con)
 ##########################################""
 
 matt_view_1 <- function() {
+    con <- dbConnect(SQLite(), "20150407.lines_sequences.sqlite")
     res <- dbSendQuery(conn=con, "
 SELECT plate_data.plate, plate_data.row, plate_data.column, plate_data.phylum, plate_data.ufid_unique, ufdb.taxon
 FROM plate_data
 JOIN dictionary_phyla_id ON dictionary_phyla_id.phylum=plate_data.phylum
 JOIN ufdb ON (ufdb.phyla_ID=dictionary_phyla_id.phyla_ID AND ufdb.uf_id = plate_data.ufid)
-JOIN plate_tracking ON plate_tracking.plate=plate_data.plate
-WHERE plate_tracking.sent_on IS '2014-05-22';")
-    dat <- fetch(res)
-    write.csv(dat, file=tempfile())
+JOIN plate_tracking ON plate_tracking.plate=plate_data.plate;")
+#WHERE plate_tracking.sent_on IS '2015-02-22';")
+    dat <- dbFetch(res) %>%
+        arrange(plate, row, column)
+    dbClearResult(res)
+    dbDisconnect(con)
+    write.csv(dat, file="/tmp/plate_matt.csv")
 }
